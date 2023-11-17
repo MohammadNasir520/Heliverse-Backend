3e31af5648272623ea1ddde2430bff97e77eaf42
MohammadNasir520 <nasirahsan520@gmail.com>
2023-11-18 01:17:56 +0600

    pagination done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,14 +3,20 @@ import catchAsync from '../../../shared/catchasync';
 import { UserService } from './user.service';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
+import { IUser } from './user.interface';
 
 const getAllUser: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
-    const result = await UserService.getAllUser();
-    sendResponse(res, {
+    const paginationOptions = pick(req.query, paginationFields);
+    const result = await UserService.getAllUser(paginationOptions);
+    sendResponse<IUser[]>(res, {
       statusCode: httpStatus.OK,
       success: true,
-      data: result,
+      data: result.data,
+      message: 'users retrieved successfully',
+      meta: result.meta,
     });
   }
 );
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,8 +1,21 @@
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { paginationHelper } from '../../helpers/paginationHelper';
 import { User } from './user.model';
 
-const getAllUser = async () => {
-  const users = await User.find();
-  return users;
+const getAllUser = async (paginationOptions: IPaginationOptions) => {
+  const { page, limit, skip } =
+    paginationHelper.calculationPagination(paginationOptions);
+  console.log(page, limit, skip);
+  const users = await User.find().skip(skip).limit(limit);
+  const total = await User.countDocuments();
+  return {
+    data: users,
+    meta: {
+      limit,
+      page,
+      total,
+    },
+  };
 };
 
 export const UserService = {
